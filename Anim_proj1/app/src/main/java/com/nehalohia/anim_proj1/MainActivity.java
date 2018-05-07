8187f05af934596bfaa9a43088fcf9870c5c0caf
Neha Lohia <24331@ITCINFOTECH.COM>
2018-05-07 15:31:54 +0530

    First Commit

---
+++ Anim_proj1/app/src/main/java/com/nehalohia/anim_proj1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nehalohia.anim_proj1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ AppData/app/src/main/java/com/nehalohia/demoapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.nehalohia.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickConvert(View view) {
+
+        EditText edittext = (EditText) findViewById(R.id.editText);
+        Double num = Double.parseDouble(edittext.getText().toString());
+        Double curr = num*65.21;
+        Log.i("Info", curr.toString());
+        Toast.makeText(MainActivity.this, curr.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
